3b1690b73e910db2161f5fecbaf86ad79d4de942
Pavel Kirpichyov <pavel.kirpichyov@gmail.com>
2020-07-13 17:20:28 +0300

    DeskClock: add increasing volume option for alarm clocks
    Based on CM12 feature:
    
    https://github.com/CyanogenMod/android_packages_apps_DeskClock/commit/7fc93e1b93b0e9e56a8426a6201a26c2bfca90ba
    http://review.cyanogenmod.org/#/c/82967/
    
    Adapt to AOSiP also
    
    Co-authored-by: Wilhelm Fitzpatrick <rafial@cyngn.com>
    Co-authored-by: Michael W <baddaemon87@gmail.com>
    Signed-off-by: Abdulwahab Isam <abdoi94.iq@gmail.com>
    
    Change-Id: Id397aafbe0d64928637680678abc845845f730e4

--- src/com/android/deskclock/provider/AlarmInstance.java
+++ src/com/android/deskclock/provider/AlarmInstance.java
@@ -33,6 +33,7 @@ import com.android.deskclock.data.DataModel;
 import java.util.Calendar;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.UUID;
 
 public final class AlarmInstance implements ClockContract.InstancesColumns {
     /**
@@ -66,7 +67,8 @@ public final class AlarmInstance implements ClockContract.InstancesColumns {
             VIBRATE,
             RINGTONE,
             ALARM_ID,
-            ALARM_STATE
+            ALARM_STATE,
+            INCREASING_VOLUME
     };
 
     /**
@@ -83,8 +85,8 @@ public final class AlarmInstance implements ClockContract.InstancesColumns {
     private static final int VIBRATE_INDEX = 7;
     private static final int RINGTONE_INDEX = 8;
     private static final int ALARM_ID_INDEX = 9;
-    private static final int ALARM_STATE_INDEX = 10;
 
+    private static final int COLUMN_COUNT = INCREASING_VOLUME_INDEX + 1;
     private static final int COLUMN_COUNT = ALARM_STATE_INDEX + 1;
 
     public static ContentValues createContentValues(AlarmInstance instance) {
@@ -109,6 +111,8 @@ public final class AlarmInstance implements ClockContract.InstancesColumns {
         }
         values.put(ALARM_ID, instance.mAlarmId);
         values.put(ALARM_STATE, instance.mAlarmState);
+        values.put(INCREASING_VOLUME, instance.mIncreasingVolume ? 1 : 0);
+
         return values;
     }
 
@@ -295,6 +299,7 @@ public final class AlarmInstance implements ClockContract.InstancesColumns {
     public Uri mRingtone;
     public Long mAlarmId;
     public int mAlarmState;
+    public boolean mIncreasingVolume;
 
     public AlarmInstance(Calendar calendar, Long alarmId) {
         this(calendar);
@@ -308,6 +313,7 @@ public final class AlarmInstance implements ClockContract.InstancesColumns {
         mVibrate = false;
         mRingtone = null;
         mAlarmState = SILENT_STATE;
+        mIncreasingVolume = false;
     }
 
     public AlarmInstance(AlarmInstance instance) {
@@ -322,6 +328,7 @@ public final class AlarmInstance implements ClockContract.InstancesColumns {
          this.mRingtone = instance.mRingtone;
          this.mAlarmId = instance.mAlarmId;
          this.mAlarmState = instance.mAlarmState;
+         this.mIncreasingVolume = instance.mIncreasingVolume;
     }
 
     public AlarmInstance(Cursor c, boolean joinedTable) {
@@ -356,6 +363,7 @@ public final class AlarmInstance implements ClockContract.InstancesColumns {
             mAlarmId = c.getLong(ALARM_ID_INDEX);
         }
         mAlarmState = c.getInt(ALARM_STATE_INDEX);
+        mIncreasingVolume = c.getInt(INCREASING_VOLUME_INDEX) == 1;
     }
 
     /**
@@ -471,6 +479,7 @@ public final class AlarmInstance implements ClockContract.InstancesColumns {
                 ", mRingtone=" + mRingtone +
                 ", mAlarmId=" + mAlarmId +
                 ", mAlarmState=" + mAlarmState +
+                ", mIncreasingVolume=" + mIncreasingVolume +
                 '}';
     }
 }
